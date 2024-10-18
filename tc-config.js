49d8931764980a318feae79c8ba95f895a395f0f
jjomp <158621541+jjomp@users.noreply.github.com>
2024-10-18 17:24:37 +0100

    Add files via upload

---
+++ tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://-nchSVz-tTon4jdS8NUlcpkaUv6kM1eL_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Módulo 1: As Empresas e a Sustentabilidade"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": ""
+};
